1d7d7aed962ece4678085e19b1118e7595f72c5a
hugotava <hugotbar@gmail.com>
2025-02-11 17:23:42 -0300

    String exercise add

--- ex_trings/index.php
+++ ex_trings/index.php
@@ -19,7 +19,7 @@
             Olá galera do $canal!
                     Tudo bem com vocês?
                 Como está sendo esse ano de $ano?
-            Abraços! \u{1F596}
+            Abraços ! \u{1F596}
         TESTE;
 
 
