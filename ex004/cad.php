407e7c84d81e19bd28ec0d15b486e2954929b9fe
Hugo Tavares <111737798+hugotava@users.noreply.github.com>
2025-02-17 15:35:27 -0300

    cad.php edition

---
+++ ex004/cad.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+</head>
+<body>
+    <header>
+        <h1>Resultado do processamento</h1>
+    </header>
+    <main>
+        <?php
+            // var_dump($_GET); // Usa-se a superglobal $_GET pois o método no index.html foi "get". Se fosse "post", deveria ser utilizada a superglobal $_POST
+            // var_dump($_REQUEST); // Junção de $_GET, $_POST e $_COOKIES
+            $n = $_GET["nome"]
+            $s = $_GET["sobrenome"]
+            echo "<p>É um prazer te conhecer, $n $s! Este é o meu site</p>"
+        ?>
+    </main>
+</body>
+</html>
