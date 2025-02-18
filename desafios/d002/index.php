90ef1c81e275883a9f1a6a14fca39f588c98b63d
hugotava <hugotbar@gmail.com>
2025-02-18 19:15:58 -0300

    Update folder d002

--- desafios/d002/index.php
+++ desafios/d002/index.php
@@ -12,7 +12,13 @@
         <h1>Trabalhando com números aleatórios</h1>
         <p>Gerando um número aleatório entre 0 e 100...</p>
        <?php 
-        $numero = rand(0,100);
+        $min = 0;
+        $max = 100;
+        $numero = mt_rand($min,$max);
+        // rand() = 1951 - Linear Congrential Generator
+        // mt_rand() = 1997 - Mersenne Twister
+        // A partir do PHP 7.1, rand() é um simples apontamento para mt_rand()
+        // random_int() gera número aleatórios criptograficamente seguros.
         echo "<p>O valor gerador foi <strong>$numero</strong></p>";
        ?>
        
