fb98f03b86342f266d39d49613ccf0b06d5dce48
hugotava <hugotbar@gmail.com>
2025-02-18 17:02:50 -0300

    ex004 update

--- ex004/cad.php
+++ ex004/cad.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
@@ -13,10 +14,11 @@
         <?php
             // var_dump($_GET); // Usa-se a superglobal $_GET pois o método no index.html foi "get". Se fosse "post", deveria ser utilizada a superglobal $_POST
             // var_dump($_REQUEST); // Junção de $_GET, $_POST e $_COOKIES
-            $n = $_GET["nome"]
-            $s = $_GET["sobrenome"]
-            echo "<p>É um prazer te conhecer, $n $s! Este é o meu site</p>"
+            $n = $_GET["nome"] ?? "sem nome";
+            $s = $_GET["sobrenome"] ?? "desconhecido";
+            echo "<p>É um prazer te conhecer, <strong>$n $s</strong>! Este é o meu site</p>";
         ?>
+        <p><a href="javascript:history.go(-1)">Voltar para  a página anterior</a></p>
     </main>
 </body>
 </html>
