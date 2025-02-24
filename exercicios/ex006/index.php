79e514303759ca0a4e250fe98ff6a74deca6682d
hugotava <hugotbar@gmail.com>
2025-02-24 13:04:51 -0300

    ex006 finished

--- exercicios/ex006/index.php
+++ exercicios/ex006/index.php
@@ -14,6 +14,7 @@
         $valor2 = $_GET['v2'] ?? 0;
     ?>
     <main>
+        <h1>Somador de Valores</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="v1">Valor 1</label>
             <input type="number" name="v1" id="v1" value="<?=$valor1?>">
@@ -21,6 +22,14 @@
             <input type="number" name="v2" id="v2" value="<?=$valor2?>">
             <input type="submit" value="Somar">
         </form>
+
+        <section id="resultado">
+            <h2>Resultado da Soma</h2>
+            <?php 
+                $soma = $valor1 + $valor2;
+                echo "<p>A soma entre os valore $valor1 e $valor2 é <strong>igual a $soma.</strong></p>"
+            ?>
+        </section>
     </main>
 </body>
 </html>
