25f9acd857d3b0d4e6b1fe071524423eb4e7895a
hugotava <hugotbar@gmail.com>
2025-02-28 16:51:38 -0300

    D008 created with index.php

---
+++ desafios/d008/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP nº 8 - Raiz quadrada e cúbica</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+    <main>
+        <h1>Informe um número</h1>
+        <form action="" method="get">
+            <label for="numero">Número</label>
+            <input type="number" name="numero" id="numero">
+            <input type="submit" value="Calcular Raízes">
+        </form>
+    </main>
+    <section>
+        <h2>Resultado final</h2>
+        <p>Analisando o <strong>número 000</strong>, temos:</p>
+        <ul>
+            <li>A sua raiz quadrada é <strong>1</strong></li>
+            <li>A sua raiz cúbica é <strong>2</strong></li>
+        </ul>
+    </section>
+</body>
+
+</html>
