d688613f10e68a1232376f096f88102371b49356
hugotava <hugotbar@gmail.com>
2025-03-02 09:13:58 -0300

    D009 created

---
+++ desafios/d009/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Desafio PHP nº 9 - Médias Aritméticas</title>
+</head>
+<body>
+    <main>
+        <h1>Médias Aritméticas</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            
+        </form>
+    </main>
+</body>
+</html>
