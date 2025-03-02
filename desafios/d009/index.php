5474584c7722a5dbf028558c32043cbd10734856
hugotava <hugotbar@gmail.com>
2025-03-02 20:21:15 -0300

    D009 html finished

--- desafios/d009/index.php
+++ desafios/d009/index.php
@@ -9,10 +9,40 @@
 </head>
 <body>
     <main>
+        <?php 
+            $valor1 = $_GET['v1'] ?? 0;
+            $peso1 = $_GET['p1'] ?? 1;
+            $valor2 = $_GET['v2'] ?? 0;
+            $peso2 = $_GET['p2'] ?? 1;
+        ?>
         <h1>Médias Aritméticas</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-            
+            <label for="v1">1º Valor</label>
+            <input type="number" id="v1" value="<?=$valor1?>">
+            <label for="p1">1º Peso</label>
+            <input type="number" id="p1" value="<?=$peso1?>">
+            <label for="v2">2º Valor</label>
+            <input type="number" id="v2" value="<?=$valor2?>">
+            <label for="p2">2º Peso</label>
+            <input type="number" id="p2" value="<?=$peso2?>">
+            <input type="submit" value="Calcular Médias">
         </form>
     </main>
+    <section>
+        <?php 
+            $msimp = ($valor1 + $valor2)/2;
+            $mpond = (($valor1 * $peso1) + ($valor2 * $peso2))/($peso1 + $peso2);
+        ?>
+        <h2>Cálculo das Médias</h2>
+        
+        <?php 
+            echo ""
+        ?>
+        <p>Analisando os valores <?=$valor1?> e <?=$valor2?>:</p>
+        <ul>
+            <li>A <strong>Média Aritmética Simples</strong> entre os valores é igual à </li>
+            <li>A <strong>Média Aritmética Ponderada</strong> com pesos X e Y é igual à </li>
+        </ul>
+    </section>
 </body>
 </html>
