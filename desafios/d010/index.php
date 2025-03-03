798bbc598e0a4a8f038748b28ae13a083478acb4
hugotava <hugotbar@gmail.com>
2025-03-03 14:20:11 -0300

    D010 finished with my solution

--- desafios/d010/index.php
+++ desafios/d010/index.php
@@ -8,11 +8,28 @@
     <title>Desafio PHP nº 10 - Cálculo da idade</title>
 </head>
 <body>
+    <?php 
+        $ano_atual = (int)date('Y');
+        $ano_nasci = ($_GET['ano1']) ?? '';
+        $ano_idade = ($_GET['ano2']) ?? $ano_atual;
+        $ano_max = $ano_atual - 1;
+    ?>
     <main>
         <h1>Calculando a sua idade</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-            
+            <label for="ano1">Em que ano você nasceu?</label>
+            <input type="number" name="ano1" id="ano1" value="<?=$ano_nasci?>" max="<?=$ano_max?>">
+            <label for="ano2">Quer saber sua idade em que ano? Atualmente estamos em <strong><?=$ano_atual?></strong></label>
+            <input type="number" name="ano2" id="ano2" value="<?=$ano_idade?>" min="<?=$ano_atual?>">
+            <input type="submit" value="Qual será minha idade?">
         </form>
     </main>
+    <section>
+        <?php 
+            $idade = (int)$ano_idade - (int)$ano_nasci;
+        ?>
+        <h2>Resultado</h2>
+        <p>Quem nasceu em <?=$ano_nasci?> vai ter <strong><?=$idade?> anos</strong> em <?=$ano_idade?>!</p>
+    </section>
 </body>
 </html>
