f65743356e1482c98270084c398c6a03bc4b3622
hugotava <hugotbar@gmail.com>
2025-03-09 18:29:42 -0300

    D012 created

---
+++ desafios/d012/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Desafio PHP nº 12 - Calculadora de Tempo</title>
+</head>
+<body>
+    <main>
+        <?php 
+            $segundos = $_GET['seg'] ?? 0;
+        ?>
+        <h1>Calculadora de Tempo</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="seg">Qual é o total de segundos?</label>
+            <input type="number" name="seg" id="seg" value="<?=$segundos?>">
+            <input type="submit" value="Calcular">
+        </form>
+    </main>
+    <section>
+        <h2>Totalizando tudo</h2>
+        <p>Analisando o valor que você digitou, <strong>[] segundos</strong> equivalem a um total de:</p>
+        <ul>
+            <li>[] semanas</li>
+            <li>[] dias</li>
+            <li>[] horas</li>
+            <li>[] minutos</li>
+            <li>[] segundos</li>
+        </ul>
+    </section>
+</body>
+</html>
