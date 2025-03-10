4e394783958f0d51f1373f0f71515526de987287
hugotava <hugotbar@gmail.com>
2025-03-10 05:44:01 -0300

    D012 add Guanabara solution and comments

--- desafios/d012/index.php
+++ desafios/d012/index.php
@@ -11,7 +11,6 @@
     <main>
         <?php 
             $segundos_total = $_GET['seg'] ?? 0;
-            
         ?>
         <h1>Calculadora de Tempo</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
@@ -28,12 +27,31 @@
             // 1 dia -> 86400 seg
             // 1 semana -> 604800 seg
 
-            // Minha solução
+            // Minha solução (variáveis no plural)
             $semanas = (int)($segundos_total / 604800);
             $dias = (int)(($segundos_total % 604800) / 86400);
             $horas = (int)((($segundos_total % 604800) % 86400) / 3600);
             $minutos = (int)(((($segundos_total % 604800) % 86400) % 3600) / 60);
             $segundos = (int)(((($segundos_total % 604800) % 86400) % 3600) % 60);
+
+            // Solução do Guanabara (variáveis no singular)
+            $sobra = $segundos_total;
+            // Total de semanas
+            $semana = (int)($sobra / 604_800);
+            $sobra = $sobra % 604_800;
+            // Total de dias
+            $dia = (int)($sobra / 86_400);
+            $sobra = $sobra % 86_400;
+            // Total de horas
+            $hora = (int)($sobra / 3_600);
+            $sobra = $sobra % 3_600;
+            // Total de minutos
+            $minuto = (int)($sobra / 60);
+            $sobra = $sobra % 60;
+            // Total de segundos
+            $segundo = $sobra;
+            
+            // A solução do prof Guanabara é mais otimizada pois vai reutilizando a variável $sobra e fazendo as atualizações conforme necessário.
         ?>
         <h2>Totalizando tudo</h2>
         <p>Analisando o valor que você digitou, <strong><?=$segundos_total?> segundos</strong> equivalem a um total de:</p>
