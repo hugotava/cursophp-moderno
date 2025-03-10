091bb4b5932f5b45b56210361b482839b3330406
hugotava <hugotbar@gmail.com>
2025-03-10 05:12:55 -0300

    D012 update with some count of seconds and minutes

--- desafios/d012/index.php
+++ desafios/d012/index.php
@@ -10,24 +10,26 @@
 <body>
     <main>
         <?php 
-            $segundos = $_GET['seg'] ?? 0;
+            $segundos_total = $_GET['seg'] ?? 0;
+            
         ?>
         <h1>Calculadora de Tempo</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="seg">Qual é o total de segundos?</label>
-            <input type="number" name="seg" id="seg" value="<?=$segundos?>">
+            <input type="number" name="seg" id="seg" value="<?=$segundos_total?>">
             <input type="submit" value="Calcular">
         </form>
     </main>
     <section>
         <?php 
-            $minutos = $segundos / 60;
-            $horas = $minutos / 60;
+            $segundos = $segundos_total % 60;
+            $minutos = intdiv($segundos_total, 60);
+            $horas = intdiv($segundos_total, 3600);
             $dias = $horas / 24;
             $semanas = $dias / 7;
         ?>
         <h2>Totalizando tudo</h2>
-        <p>Analisando o valor que você digitou, <strong><?=$segundos?> segundos</strong> equivalem a um total de:</p>
+        <p>Analisando o valor que você digitou, <strong><?=$segundos_total?> segundos</strong> equivalem a um total de:</p>
         <ul>
             <li><?=(int)$semanas?> semanas</li>
             <li><?=(int)$dias?> dias</li>
